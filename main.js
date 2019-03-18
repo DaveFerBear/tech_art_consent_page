cbaa7ac24a2439c7cad2ae2732d3c0688b18879d
David Ferris <david.j.ferris.21@gmail.com>
2019-03-18 15:12:35 -0400

    Add live video demo.

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // Probably some javascript should go here.
+$(document).ready(function () {
+    var video = document.querySelector("#videoElement");
+
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+                video.srcObject = stream;
+            })
+            .catch(function (err0r) {
+                console.log("Something went wrong!");
+            });
+    }
+});
