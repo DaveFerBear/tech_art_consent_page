d4b3ba56456307f7296dbc30c42ce671b083e099
David Ferris <david.j.ferris.21@gmail.com>
2019-03-19 13:47:56 -0400

    Working RecordRTC demo

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             .then(function (stream) {
                 video.srcObject = stream;
             })
-            .catch(function (err0r) {
+            .catch(function (error) {
                 console.log("Something went wrong!");
             });
     }
