816ed4539f8bf6472acd1c708890cbf0f1544a9d
David Ferris <david.j.ferris.21@gmail.com>
2019-03-28 14:27:40 -0400

    Working on test-video-capture experiment

--- main.js
+++ main.js
@@ -15,6 +15,44 @@ function changePage(page) {
     }
 }
 
+function videoSetup() {
+    let shouldStop = false;
+    let stopped = false;
+    const downloadLink = document.getElementById('download');
+    const stopButton = document.getElementById('stop');
+
+    stopButton.addEventListener('click', function () {
+        shouldStop = true;
+    })
+
+    var handleSuccess = function (stream) {
+        const options = { mimeType: 'video/webm' };
+        const recordedChunks = [];
+        const mediaRecorder = new MediaRecorder(stream, options);
+
+        mediaRecorder.addEventListener('dataavailable', function (e) {
+            if (e.data.size > 0) {
+                recordedChunks.push(e.data);
+            }
+
+            if (shouldStop === true && stopped === false) {
+                mediaRecorder.stop();
+                stopped = true;
+            }
+        });
+
+        mediaRecorder.addEventListener('stop', function () {
+            downloadLink.href = URL.createObjectURL(new Blob(recordedChunks));
+            downloadLink.download = 'acetest.webm';
+        });
+
+        mediaRecorder.start();
+    };
+
+    navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+        .then(handleSuccess);
+}
+
 $(document).ready(function () {
     // Hide Scrollbars (no overflow)
     $("body").css("overflow", "hidden");
@@ -27,6 +65,7 @@ $(document).ready(function () {
         changePage(3);
     });
 
+    // videoSetup();
 
     var video = document.querySelector("#videoElement");
 
