3e2151b0fd2541c4c87253ca5dd0954f4d1e3dd2
David Ferris <david.j.ferris.21@gmail.com>
2019-03-29 15:22:03 -0400

    Loading image src on htlm5 canvas

--- microtiles_client/draw.js
+++ microtiles_client/draw.js
@@ -9,12 +9,34 @@ function drawRect(ctx) {
     ctx.strokeRect(50, 50, 50, 50);
 }
 
+function drawImage(ctx, link) {
+    var img = new Image();
+    img.onload = function () {
+        ctx.drawImage(img, 0, 0);
+    };
+    img.src = link;
+}
+
+// ImageId is 1 through 5
+function getUserImageMediaLink(userId, imageId) {
+    // TODO: add wherever the serverless functionw writes to.
+    var endpoint = "https://www.googleapis.com/storage/v1/b/gene499-bucket-v2/o/test_file_consent"
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", endpoint, false); // False for synchronous request.
+    xhr.send(null);
+    return JSON.parse(xhr.responseText).mediaLink;
+}
+
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 
     canvas = document.getElementById("myCanvas");
     context = canvas.getContext("2d");
 
+    var mediaLink = getUserImageMediaLink(69, 69);
+
+    drawImage(context, mediaLink);
     drawText(context);
     drawRect(context);
+
 });
