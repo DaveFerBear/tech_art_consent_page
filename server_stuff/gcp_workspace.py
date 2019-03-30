39bcee9b1bdbd3ffac6b6d03d35c56a7a42e0ef6
Rowan Ferrabee <rowanferrabee@gmail.com>
2019-03-30 16:41:25 -0400

    hack the fuck out of this whole thing

--- server_stuff/gcp_workspace.py
+++ server_stuff/gcp_workspace.py
@@ -3,7 +3,10 @@ import os
 import cv2
 
 BASE_URL = 'https://www.googleapis.com/storage/v1/b/gene499-bucket-v2/o/'
-
+min_face_width = 100
+min_face_height = 100
+avg_face_width = 160
+avg_face_height = 160
 
 def fetch_blob_through_medialink(url):
     print("FETCHING BLOB: {}".format(url))
@@ -48,32 +51,91 @@ def process_image(filename, did_consent):
         cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
     img = cv2.imread(filename)
     height, width = img.shape[:-1]
-
-    if (12*width > 14*height):
-        # Crop out middle width
-        access_width = (12*width - 14*height) / 12
-        img = img[:, int(access_width/2):int(-access_width/2)]
-        # img = cv2.resize(img, (des_width, des_height), interpolation=cv2.INTER_LINEAR)
-
-    elif (12*width < 14*height):
-        # Crop out middle height
-        access_height = (14*height - 12*width) / 14
-        img = img[int(access_height/2):int(-access_height/2), :]
-        # img = cv2.resize(img, (des_width, des_height), interpolation=cv2.INTER_LINEAR)
-
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     if not(did_consent):
         img = gray
+        if (12*width < 14*height):
+            access_height = (14*height - 12*width) / 14
+            img = img[int(access_height/2):int(-access_height/2), :]
+
         new_size = (int(42), int(36))
         orig_size = tuple(img.shape[::-1])
         img = cv2.resize(img, new_size, interpolation=cv2.INTER_LINEAR)
         img = cv2.resize(img, orig_size, interpolation=cv2.INTER_NEAREST)
     else:
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        if faces is not None and len(faces) > 0:
-            (x, y, w, h) = faces[0]
-            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 4)
+        faces = face_cascade.detectMultiScale(gray, 1.01, 2)
+        faces = [face for face in faces if face[2] > min_face_width and face[3] > min_face_height]
+        print(faces)
+
+        if (len(faces) == 0):
+            face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt.xml")
+            faces = face_cascade.detectMultiScale(gray, 1.1, 1)
+            faces = [face for face in faces if face[2] > min_face_width and face[3] > min_face_height]
+            print(faces)
+
+        if (len(faces) == 0):
+            face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt2.xml")
+            faces = face_cascade.detectMultiScale(gray, 1.1, 1)
+            faces = [face for face in faces if face[2] > min_face_width and face[3] > min_face_height]
+            print(faces)
+
+        if (len(faces) == 0):
+            face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt_tree.xml")
+            faces = face_cascade.detectMultiScale(gray, 1.01, 2)
+            faces = [face for face in faces if face[2] > min_face_width and face[3] > min_face_height]
+            print(faces)
+
+        if (len(faces) == 0):
+            glasses_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye_tree_eyeglasses.xml")
+            glasses = glasses_cascade.detectMultiScale(gray, 1.3, 3)
+            # Hail mary
+            if (len(glasses) == 2):
+                glasses = list(glasses)
+                # Sort glasses left to right
+                glasses.sort(key=lambda item: item[0])
+                (x1, y1, w1, h1) = glasses[0]
+                (x2, y2, w2, h2) = glasses[1]
+                net_width = x2+w2-x1
+                if (w1 < 90 and w2 < 90 and net_width > min_face_width):
+                    access_width = net_width - avg_face_width
+                    faces = [[int(x1 + access_width/2), int(y1-avg_face_width/3), avg_face_width, avg_face_height]]
+
+                for i, glass in enumerate(glasses):
+                    (x, y, w, h) = glass
+                    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 4)
+
+        if (len(faces) == 0):
+            if (12*width < 14*height):
+                access_height = (14*height - 12*width) / 14
+                img = img[int(access_height/2):int(-access_height/2), :]
+
+        min_diff = 100000000
+        best_guy = -1
+        for i, face in enumerate(faces):
+            (x, y, w, h) = face
+            diff = x - (width - x - w)
+            print(diff)
+            if (abs(diff) < min_diff):
+                best_guy = i
+                min_diff = abs(diff)
+
+        for i, face in enumerate(faces):
+            (x, y, w, h) = face
+            color = 0
+            if (i == best_guy):
+                color = 255
+            cv2.rectangle(img, (x, y), (x+w, y+h), (color, 0, 255), 4)
+            if (i == best_guy and 12*width < 14*height):
+                access_height = (14*height - 12*width) / 14
+                top_dist = y
+                bottom_dist = height - y - h
+                if (top_dist > access_height):
+                    img = img[int(access_height):,:]
+                elif (bottom_dist > access_height):
+                    img = img[:int(-access_height),:]
+                else:
+                    img = img[int(access_height/2):int(-access_height/2), :]
 
     # take processed off front.
     processed_fname = 'tmp/processed-'+filename.split('/')[-1]
