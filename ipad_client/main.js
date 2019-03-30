551845e39b0333756a6713d92453d03a1fae1406
David Ferris <david.j.ferris.21@gmail.com>
2019-03-30 13:41:31 -0400

    Ingegrated the 2 pages

--- ipad_client/main.js
+++ ipad_client/main.js
@@ -23,49 +23,124 @@ function changePage(page) {
 function animateEye(i) {
     EYE_FLICKER_MS = 3000;
     document.getElementById("eye").src = "img/eye-v2-" + i + ".png";
-    console.log(i);
     setTimeout(function () {
         animateEye((i) % 4 + 1);
     }, EYE_FLICKER_MS);
 }
 
-function videoSetup() {
-    let shouldStop = false;
-    let stopped = false;
-    const downloadLink = document.getElementById('download');
-    const stopButton = document.getElementById('stop');
-
-    stopButton.addEventListener('click', function () {
-        shouldStop = true;
-    })
-
-    var handleSuccess = function (stream) {
-        const options = { mimeType: 'video/webm' };
-        const recordedChunks = [];
-        const mediaRecorder = new MediaRecorder(stream, options);
-
-        mediaRecorder.addEventListener('dataavailable', function (e) {
-            if (e.data.size > 0) {
-                recordedChunks.push(e.data);
-            }
-
-            if (shouldStop === true && stopped === false) {
-                mediaRecorder.stop();
-                stopped = true;
-            }
-        });
-
-        mediaRecorder.addEventListener('stop', function () {
-            downloadLink.href = URL.createObjectURL(new Blob(recordedChunks));
-            downloadLink.download = 'acetest.webm';
-        });
-
-        mediaRecorder.start();
-    };
-
-    navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-        .then(handleSuccess);
+function kickOff() {
+    takeASnap().then(uploadBlobToCloud);
 }
+
+function recursiveDelay(functionToCall, executionsNumber, timeoutInMilliseconds) {
+    if (executionsNumber) { //exit condition
+        functionToCall(); // external function execution
+        setTimeout(
+            () => {
+                recursiveDelay(functionToCall, executionsNumber - 1, timeoutInMilliseconds); //recursive call
+            }, timeoutInMilliseconds);
+    }
+}
+
+function takeASnap() {
+    const canvas = document.createElement('canvas'); // create a canvas
+    const ctx = canvas.getContext('2d'); // get its context
+    canvas.width = vid.videoWidth; // set its size to the one of the video
+    canvas.height = vid.videoHeight;
+    ctx.drawImage(vid, 0, 0); // the video
+
+    return new Promise((res, rej) => {
+        canvas.toBlob(res, 'image/jpeg'); // request a Blob from the canvas
+    });
+
+}
+
+function uploadBlobToCloud(blob) {
+    // Some logging and housekeeping.
+    imageId++;
+    if (imageId > NUM_IMAGES) {
+        imageId = 1
+    };
+    console.log(userId + "/" + imageId);
+    console.log(blob);
+
+    // Upload to cloud storage.
+    function formatParams(params) {
+        return "?" + Object
+            .keys(params)
+            .map(function (key) {
+                return key + "=" + encodeURIComponent(params[key])
+            })
+            .join("&")
+    }
+
+    var fileName = "raw/" + userId + "/" + imageId;
+    var uploadEndpoint = "https://www.googleapis.com/upload/storage/v1/b/gene499-bucket-v2/o";
+    var params = {
+        name: fileName,
+        uploadType: "media"
+    }
+    var url = uploadEndpoint + formatParams(params);
+
+    var xhr = new XMLHttpRequest();
+    var synchronousRequest = (imageId == NUM_IMAGES); // Only run final request synchronously.
+    xhr.open("POST", url, synchronousRequest);
+
+    //Send the proper header information along with the request
+    // xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () { // Call a function when the state changes.
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            console.log("Request Successful.")
+        }
+    }
+    xhr.send(blob); // Send the blob!
+
+    if (imageId == NUM_IMAGES) {
+        var xhr2 = new XMLHttpRequest();
+        // Fetch mediaLink from metadata
+        var manifestEndpoint = "https://www.googleapis.com/storage/v1/b/gene499-bucket-v2/o/manifest.txt"
+        xhr2.open("GET", manifestEndpoint, false); // False for synchronous request.
+        xhr2.send(null);
+        var mediaLink = JSON.parse(xhr2.responseText).mediaLink;
+
+        // Fetch data from mediaLink
+        xhr2.open("GET", mediaLink, false); // False for synchronous request.
+        xhr2.send(null);
+
+        var manifest;
+        if (!xhr2.responseText) {
+            manifest = userId + ',1';
+        } else {
+            manifest = xhr2.responseText + "\n" + userId + ',1';
+        }
+
+        console.log(manifest);
+
+        // Upload appended manifest
+        var manifestParams = {
+            name: "manifest.txt",
+            uploadType: "media"
+        }
+        var manifestUrl = uploadEndpoint + formatParams(manifestParams)
+        xhr2.open("POST", manifestUrl, true);
+        xhr2.onreadystatechange = function () { // Call a function when the state changes.
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                console.log("MANIFEST Request Successful.")
+            }
+        }
+        xhr2.send(manifest); // Send the blob!
+    }
+}
+
+
+////////////////        ON DOM LOADED           /////////////////
+
+var userId = 0000;
+var imageId = 0;
+
+var NUM_IMAGES = 5;
+var IMAGE_DELAY = 1000;
+var vid;
 
 $(document).ready(function () {
     // Hide Scrollbars (no overflow)
@@ -81,17 +156,20 @@ $(document).ready(function () {
 
     animateEye(1);
 
-    // videoSetup();
+    vid = document.querySelector('video');
+    navigator.mediaDevices.getUserMedia({ video: true }) // request cam
+        .then(stream => {
+            vid.srcObject = stream; // don't use createObjectURL(MediaStream)
+            return vid.play(); // returns a Promise
+        })
+        .then(() => { // enable the button
+            const btn = document.getElementById('page-2-button');
+            btn.disabled = false;
+            btn.onclick = e => {
+                imageId = 0;
+                userId = Math.floor(Math.random() * 10000);
+                recursiveDelay(kickOff, NUM_IMAGES, IMAGE_DELAY)
 
-    var video = document.querySelector("#videoElement");
-
-    if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then(function (stream) {
-                video.srcObject = stream;
-            })
-            .catch(function (error) {
-                console.log("Something went wrong!");
-            });
-    }
+            };
+        });
 });
